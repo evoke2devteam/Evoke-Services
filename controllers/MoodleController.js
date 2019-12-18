7dd48f541f2f36527229c00f826ad34e6cd793ef
Camilo Reyes <alondres15@gmail.com>
2019-12-18 12:29:49 -0500

    ok, another change

--- controllers/MoodleController.js
+++ controllers/MoodleController.js
@@ -69,7 +69,8 @@ async function listOfStatusUserByCourse(req, res) {
                         for (let i = 0; i < courses.length; i++) {
                             for (let j = 0; j < courses[i].statuses.length; j++) {
                                 //const element = array[j];
-                                let name1 = activitiesInfo.map(e => { return e; }).indexOf(courses[i].statuses[j].cmid);
+                                //let name1 = activitiesInfo.map(e => { return e; }).indexOf(courses[i].statuses[j].cmid);
+                                let name1 = activitiesInfo.findIndex(x => x.id === courses[i].statuses[j].cmid);
                                 console.log(name1);
                                 courses[i].statuses[j].reward = data2[j].Reward;
                                 courses[i].statuses[j].paid_status = paid[iter].paid_status;
